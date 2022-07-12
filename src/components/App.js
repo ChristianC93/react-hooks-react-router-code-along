143c1f3b2244fd3ac1590d459ffb581afed5ff9b
Christian Castillo <castillochristian3@gmail.com>
2022-07-12 10:57:07 -0400

    commit tree a5dad2395c1ed64c39b5654d7ffb12dc0e845d26

---
+++ src/components/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {Route, Switch} from "react-router-dom";
+import NavBar from "./Navbar";
+import About from "./About";
+import Login from "./Login";
+import Home from "./Home";
+
+
+
+function App() {
+    return (
+      <div>
+        <NavBar />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    );
+}
+
+export default App;
